55d56eb6ee4907e6aaf2e80f8ed4288426eb84e8
Anuo <2747146768@qq.com>
2020-05-22 01:02:28 +0800

    add view of the square and change the view of the mine

---
+++ app/src/main/java/com/cflower/doitnow/model/mine/MineItem.kt
@@ -0,0 +1,8 @@
+package com.cflower.doitnow.model.mine
+
+import com.cflower.doitnow.R
+
+data class MineItem (
+    val img: Int = R.drawable.bg_main_mine,
+    val name: String = "我的世界"
+)
--- app/src/main/java/com/cflower/doitnow/ui/fragment/MineFragment.kt
+++ app/src/main/java/com/cflower/doitnow/ui/fragment/MineFragment.kt
@@ -7,12 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.cflower.doitnow.R
 
-class MineFragment :Fragment(){
+class MineFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main_square,container,false);
+        return inflater.inflate(R.layout.fragment_main_mine, container, false);
     }
+
 }
